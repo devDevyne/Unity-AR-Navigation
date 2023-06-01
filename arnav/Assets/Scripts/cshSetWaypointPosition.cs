65ac32422a8c64926ca35d5b3f3a202e570b60ba
devynean <devyne177@gmail.com>
2023-06-01 19:29:23 +0900

    First Commit

---
+++ arnav/Assets/Scripts/cshSetWaypointPosition.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cshSetWaypointPosition : MonoBehaviour
+{
+    // Lat and Lon values for the desired position 
+    public float latitude;
+    public float longitude;
+
+    // Conversion scale factors to adjust the position in Unity
+    public float latitudeScale = 1f;
+    public float longitudeScale = 1f;
+    public float height = 0f;
+
+    void Start()
+    {
+        // Convert lat and lon to Unity coordinates
+        float unityX = longitude * longitudeScale;
+        float unityZ = latitude * latitudeScale;
+
+        //Set the game object's position
+        transform.position = new Vector3(unityX, height, unityZ);
+    }
+}
